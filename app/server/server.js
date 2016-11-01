1445fed617925e7be7f12a839c14f3696105b7d4
JAMES O MEARA(26-10-2016 00-24 <danu7_oj2@danu7>
2016-11-01 23:27:51 +0000

    fixed server reading files from public folder

--- app/server/server.js
+++ app/server/server.js
@@ -5,13 +5,15 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../client/src');
 
-app.use(express.static(path.join(__dirname, '/../../../FinalyearProject')));
+
+
+app.use(express.static(path.join(__dirname, '/../client/public')));
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!');
+app.listen(3333, function () {
+  console.log('Server listening on port 3333!');
 });
 
